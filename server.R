7e48eb574c94511784eccd8aa87c423db0cb7792
Kaunitz <Linda.Kaunitz@spectrumk.de>
2020-01-14 17:49:09 +0100

    update code (Farbe, Theme, PLZ-Stellen)

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
-library(data.table)
+#https://www.suche-postleitzahl.org/downloads 
+#library(data.table)
 library(sf)
 library(dplyr)
 library(ggplot2)
@@ -11,7 +12,6 @@ server <- function(input, output) {
   observeEvent(input$file1, {
     
     # Derzeit muss dieser Block noch oben sthen, damit die Karte n a c h dem Upload neu erstellt wird
-    # Teständerung
     data <<- data.frame(read.csv2(input$file1$datapath))
     replaceData(proxy, data, resetPaging = FALSE)  # Quelle: https://github.com/rstudio/DT/pull/480, wenn man eine ganze Zeile löscht, gibt es noch nen Error
   })
@@ -22,22 +22,76 @@ server <- function(input, output) {
     
     input$daten_cell_edit # Hiermit wird die Karte neu gerendert, wenn die Tabelle editiert wird
     input$file1 # Hiermit wird die Karte neu gerendert, wenn Daten hochgeladen werden
+   
+    # zeichen <- unique(nchar(data$plz))
+    # if(zeichen=="1"){
+    #   file_load      <- sf::st_read("data/plz-1stellig.shp")
+    #   file      <- merge(file_load, data, by = "plz", all=FALSE)
+    # }
+    # if(zeichen=="2" & nrow(file)!=0){
+    #   file_load      <- sf::st_read("data/plz-2stellig.shp")
+    #   file      <- merge(file_load, file, by = "plz", all=FALSE)
+    # }
+    # else if(zeichen=="2" & nrow(file)==0){
+    #   file_load      <- sf::st_read("data/plz-2stellig.shp")
+    #   file      <- merge(file_load, data, by = "plz")
+    # }
     
-    
+    if(max(nchar(data$plz))=="1"){
     file      <- sf::st_read("data/plz-1stellig.shp")
+    }
+    else if(max(nchar(data$plz))=="2"){
+      file      <- sf::st_read("data/plz-2stellig.shp")
+    }
+    else if(max(nchar(data$plz))=="3"){
+      file      <- sf::st_read("data/plz-3stellig.shp")
+    }
     
+    #merge Polygen-Daten und Daten
     file      <- merge(file, data, by = "plz")
-    file.sf   <- file['plz']
     
+    
+    #Farbe
+    if(input$color_choice=="blau"){
+      color_karte <- "cividis"
+    }
+      else if(input$color_choice=="rot"){
+        color_karte <- "inferno"
+      }
+        else if(input$color_choice=="grün"){
+          color_karte <- "viridis"
+        }
+          else if(input$color_choice=="lila"){
+            color_karte <- "plasma"
+          }
+    #Theme, https://ggplot2.tidyverse.org/reference/ggtheme.html
+    if(input$theme_choice=="default"){
+    theme_karte <- theme_bw()
+    }
+      else if(input$theme_choice=="minimal"){
+        theme_karte <- theme_minimal()
+      }
+        else if(input$theme_choice=="grau"){
+          theme_karte <- theme_gray()
+        }
+          else if(input$theme_choice=="dunkel"){
+            theme_karte <- theme_dark()
+          }
+      
+    
+    #Karte generieren
     file %>%
       ggplot() + 
-      geom_sf(data = file.sf) + 
+      geom_sf(data = file) + 
       geom_sf(aes(fill = N)) + 
-      scale_fill_viridis("N", option = "cividis", direction = -1) + 
-      theme_bw() + 
+      scale_fill_viridis("N", option = color_karte, direction = -1) + 
+      theme_karte + 
       ggtitle("Deutschlandkarte")
     
+
   })
+  
+  
   
   output$daten  <-  renderDT(data, selection = 'none', editable = TRUE)
   
--- ui.R
+++ ui.R
@@ -2,28 +2,42 @@ fluidPage(
   titlePanel("PLZ Karte"),
   
   fluidRow(
-    column(1
-    ),
+    column(1),
     
-    column(4,
+    column(3,
            # Input: Select a file ----
-           fileInput("file1", "CSV Datei hochladen",
+           fileInput("file1", "CSV Datei hochladen (plz, N)",
                      multiple = FALSE,
                      accept = c("text/csv",
                                 "text/comma-separated-values,text/plain",
                                 ".csv")),
+           hr(),
+           helpText("Zur Zeit nur PLZ 1,2,3-Steller möglich"),
            DT::dataTableOutput("daten")
            ),
-    
+    #Dropdown einfügen Farbe
+    sidebarPanel(
+      selectInput("color_choice", "Farbe:",
+                  choices=c("blau", "rot","grün","lila"))#,
+      # hr(),
+      # helpText("Bitte Waehle die Anzahl der Ziffern fuer die PLZ")
+    ),
+    #Dropdown einfügen Theme
+    sidebarPanel(
+      selectInput("theme_choice", "Theme:",
+                  choices=c("default", "minimal","grau", "dunkel"))#,
+      # hr(),
+      # helpText("Bitte Waehle die Anzahl der Ziffern fuer die PLZ")
+    ),
     column(1),
-    
-    column(5,
+
+    column(8,
            plotOutput(outputId = "karte")
            ),
     
     column(1)
     
-    
+
     
   )
   
